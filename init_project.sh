e662ba3be912eba2154dbadf391f8a972b9864cb
patrickryan <theyoungsoul@gmail.com>
2022-06-05 13:06:29 -0500

    updated init script

--- init_project.sh
+++ init_project.sh
@@ -1,5 +1,5 @@
 docker-compose build django-web
-docker-compose exec django-web django-admin startproject django_project .
+docker-compose run django-web django-admin startproject django_project .
 docker-compose up -d --build
 docker-compose exec django-web python manage.py migrate
 docker-compose exec django-web python manage.py createsuperuser
