c85ddb4a0a5e8264484759a8d3cff320db7ebbb2
patrickryan <theyoungsoul@gmail.com>
2022-06-09 13:07:41 -0500

    remove the migrate and superuser from init

--- init_project.sh
+++ init_project.sh
@@ -1,6 +1,8 @@
 docker-compose build django-web
 docker-compose run django-web django-admin startproject django_project .
 docker-compose up -d --build
-docker-compose exec django-web python manage.py migrate
-docker-compose exec django-web python manage.py createsuperuser
-
+# NOTE you want to create a custom user FIRST, before doing a migrate and creating a super user
+# the statements below are kept in the init script as an example but you should not run these before
+# you create a custom user model
+#docker-compose exec django-web python manage.py migrate
+#docker-compose exec django-web python manage.py createsuperuser
