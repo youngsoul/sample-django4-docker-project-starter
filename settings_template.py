f029549036f9b84c49273a16a53671fdd8e69eca
patrickryan <theyoungsoul@gmail.com>
2022-09-27 19:35:20 -0500

    fixed config and settings template

--- settings_template.py
+++ settings_template.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from environs import Env
 env = Env() # new
 
-env.read_env("../.env.django") # new
+env.read_env(".env.django") # new
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,12 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 3rd party
-    'rest_framework',
-
-    # Local
-    'movies.apps.MoviesConfig',
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -132,12 +126,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'accounts.CustomUser'
-
-if not DEBUG:
-    REST_FRAMEWORK = {
-        "DEFAULT_RENDERER_CLASSES": (
-            "rest_framework.renderers.JSONRenderer",
-        )
-    }
